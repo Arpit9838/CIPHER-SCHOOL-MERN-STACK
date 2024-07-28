1069ee205682f87715db8e964b6648c1674dd421
Arpit Mishra <117110049+Arpit9838@users.noreply.github.com>
2024-07-28 10:57:34 +0530

    Add files via upload

---
+++ Lecture31/test/connection.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const URL =
+  "mongodb+srv://new-user-123:new-user-123@testcluster.vyftd.mongodb.net/test?retryWrites=true&w=majority";
+
+before((done) => {
+  mongoose.connect(URL);
+  // Event driven node.js
+  // Asynchronous
+  mongoose.connection
+    .once("open", () => {
+      console.log("Connection to the Database successful");
+      done();
+    })
+    .on("error", (error) => {
+      console.log("Unable to connect", error);
+    });
+});
+
+// Drop my collections before each test
+beforeEach((done) => {
+  mongoose.connection.collections.people.drop(() => {
+    done();
+  });
+});
