f84bd5c6bcf8e7e79f0079c5875aa1d1993a7313
Arpit Mishra <117110049+Arpit9838@users.noreply.github.com>
2024-07-28 10:47:54 +0530

    Add files via upload

---
+++ Lecture-23/AddPizza.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AddPizza() {
+  return (
+    <div>
+        <input type='text'>name</input>
+        <input type='password'>Password</input>
+        <button>Add card</button>
+    </div>
+  )
+}
+
+export default AddPizza
---
+++ Lecture-23/App.js
@@ -0,0 +1,57 @@
+function App() {
+    return (
+        <>
+              <header>
+            this is header
+        </header>
+        <p>
+            this is the para
+        </p>
+        </>
+  
+    )
+    class App extends Component {
+        state = {
+            name: "react",
+            description: "react is very good",
+            description2: "this is an frontend library",
+            types: [
+                {name: "state", useState: "useEffect"},
+                {name: "props", useState: "useContext"},
+                {name: "component", useState: "useRef"},
+                {name: "context", useState: "useReducer"},    
+            ]
+        }
+        render() {
+            let dynamicName = 'React is very good'
+            let brand = 'dominos'
+            let price = 220
+            return (
+                <>
+                  <h1>this is class component</h1>
+               <h1>
+                {dynamicName}
+                {brand}
+                {price}
+               </h1> 
+               <div class='container'>
+                {this.state.types.map((type) => {
+                    return <h3>{type.name}</h3>
+                })}
+               </div>
+               <div>
+                 {/* conditional rendering  */}
+                {this.state.name === "react" ? <h1>yes</h1> : <h1>no</h1>}
+               </div>
+               <div>
+                 {/* ternary operator  */}
+                {this.state.name === "react" ? "yes" : "no"}
+                
+               </div>
+                </>
+              
+            )
+        }
+    }
+    // export default App;
+}
